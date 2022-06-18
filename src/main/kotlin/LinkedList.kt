b87c606bf3eae682a410df2e0747a55fc9a9f46c
m.badrmiran <masoud.badremiran@gmail.com>
2022-06-18 16:36:05 +0430

    Push and append methods were added

--- src/main/kotlin/LinkedList.kt
+++ src/main/kotlin/LinkedList.kt
@@ -10,4 +10,24 @@ class LinkedList<T> {
         else head.toString()
     }
 
+    fun push(value : T): LinkedList<T> {
+        head = Node(value,head)
+        if (tail == null)
+            tail = head
+        size++
+        return this
+    }
+
+    fun append(value: T): LinkedList<T> {
+        if (isEmpty()){
+            push(value)
+            return this
+        }
+
+        tail?.next = Node(value)
+        tail = tail?.next
+        size++
+        return this
+    }
+
 }
